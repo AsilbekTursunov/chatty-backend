7d73a852f9bb1c4790bd0f3e199adfa63a1e37d8
asilbek_tursun <148640148+AsilbekTursunov@users.noreply.github.com>
2025-07-07 06:57:17 +0500

    add vercel url

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173", "*"],
+    origin: 'https://chatty-video-call.vercel.app/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true, // allow frontend to send cookies
   })
