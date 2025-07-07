2e5c24a02ce8f4e5a7d69b4e7c62b6c7edd41c43
asilbek_tursun <148640148+AsilbekTursunov@users.noreply.github.com>
2025-07-07 08:43:10 +0500

    edit api

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'https://chatty-video-call.vercel.app' || 'http://localhost:5173',
+    origin: 'http://localhost:5173',
     credentials: true, // allow frontend to send cookies
   })
 );
